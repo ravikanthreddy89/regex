62a2f0c6c9eb3d615852dbc5fd1aa172c9e89338
ravikanth <ravikanthreddy89@gmail.com>
2013-12-25 15:49:27 -0500

    Email extraction done.

--- re_match_test.py
+++ re_match_test.py
@@ -2,10 +2,9 @@ import re;
 import sys;
 
 fp=open(sys.argv[1]);
-
 for line in fp:
-	matchObj=re.search('www\..*\.edu', line);
+	matchObj=re.findall(u"[\w.!#$%&*+'-/=?^_`{|}~\"\\]*@\w[\w.-]*\w\.\w[\w.-]*\w", line);
 	if matchObj:
-		print matchObj.group();
+		print matchObj;
 
 
